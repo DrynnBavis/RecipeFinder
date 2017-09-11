5a8dac1a7c0b0e0cde422ec0b3400238bdc1327c
drynnbavis <drynnbavis@gmail.com>
2017-09-11 14:32:44 -0400

    added OpenShift details

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -8,6 +10,6 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
-app.listen(8080, () => {
-	console.log('Listening to 8080!');
+app.listen(server_port, server_ip_address () => {
+	console.log('Listening on' + server_ip_address +' at port ' + server_port);
 });
